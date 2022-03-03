355555ef0b7e6cba430217233084662bf6c9d3ac
DMangos <53335172+DMangos@users.noreply.github.com>
2022-03-03 08:39:09 +0000

    Update fortune_cookie.py

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -31,8 +31,13 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
 
     The message should include the user's fortune and lucky numbers.
     """
+    print(generate_lucky_numbers(how_many_lucky_numbers))
+    generate_fortune()
+    
+    return generate_fortune()    
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
+
     # cookie's message.
 
     raise NotImplementedError()
