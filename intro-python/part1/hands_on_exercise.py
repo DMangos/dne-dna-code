e90e10e243d5634cc1a3b965a4601ce2a960072e
DMangos <53335172+DMangos@users.noreply.github.com>
2022-03-04 08:36:56 +0000

    Update hands_on_exercise.py

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,18 +7,56 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
+print(pi,type(pi))
 
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
+if i<50:
+  print(i,"less than fifty")
+elif i>50:
+  print(i, "greater than 50")
+else:
+  print(i)
+
 
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
 
+if picked_fruit=="orange":
+  fruit_colour="orange"
+elif picked_fruit=="strawberry":
+  fruit_colour="red"
+else:
+  fruit_colour="yellow"
+  
+print(fruit_colour)
+
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
+def multip(x,y):
+  z=x*y
+  print("The multiplication of", x, " times", y , " equals",z)
+ 
+
+multip(12, 96)
+
+multip(48,17)
+
+multip(196523, 87323)
+
+
+def multip():
+    x=int(input("give an integer x"))
+    y=int(input("give an integer y"))
+    z=x*y
+    print("The multiplication of", x, " times", y , " equals",z)
+ 
+
+multip()
+
 
 
 # TODO: Now call the function a few times to calculate the following answers
